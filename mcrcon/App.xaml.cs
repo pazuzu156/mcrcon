dc55634a4fa1c77d5d9205c672c8beeae34ffa67
Kaleb Klein <klein.jae@gmail.com>
2016-09-15 23:22:23 -0400

    Removed my rcon library in place for SourceRcon for now
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- mcrcon/App.xaml.cs
+++ mcrcon/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
+using System.Globalization;
+using System.Reflection;
 
 namespace mcrcon
 {
@@ -13,5 +16,30 @@ namespace mcrcon
 	/// </summary>
 	public partial class App : Application
 	{
+		Mutex _mutex;
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			string mutexID = string.Format(CultureInfo.InvariantCulture, "Local\\{{{0}}}{{{1}}}",
+				assembly.GetType().GUID, assembly.GetName().Name);
+			bool mutexCreated;
+
+			_mutex = new Mutex(true, mutexID, out mutexCreated);
+
+			if(!mutexCreated)
+			{
+				_mutex = null;
+				MessageBox.Show("You can only have one instance of mcrcon running at a time!", "Multiple Instance Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				Current.Shutdown(-2);
+				return;
+			}
+		}
+
+		private void Application_Exit(object sender, ExitEventArgs e)
+		{
+			_mutex = null;
+			Current.Shutdown(0);
+		}
 	}
 }
